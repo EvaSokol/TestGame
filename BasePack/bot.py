e16ac8914a8b1031bfce650d261a3d0c6f0d7b3e
EvaSokolyanskaya <esokolyanskaya@lohika.com>
2014-11-07 15:53:59 +0200

    First steps

---
+++ BasePack/bot.py
@@ -0,0 +1,21 @@
+'''
+Created on Nov 7, 2014
+
+@author: esokolyanskaya
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+    def prn(self): 
+        a = self.GET_ACTIVE_ELEMENT
+        print (a)
+
+prn(self)  
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
