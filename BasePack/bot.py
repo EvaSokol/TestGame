80d64cb17eb3c8974884b895ef8e0d9685bfa30f
EvaSokolyanskaya <esokolyanskaya@lohika.com>
2014-11-10 18:31:32 +0200

    Steps analysis is added

--- BasePack/bot.py
+++ BasePack/bot.py
@@ -5,51 +5,100 @@ Created on Nov 7, 2014
 '''
 import random
 
-class BullsAndCows():
+class BullsAndCows(object):
                     
     def __init__(self):
-        digits = 5
-        ourfile = "text.txt"
-        mynum = self.generator(digits)
-        self.writefile(mynum, ourfile)
-        self.getsize(ourfile)
-        self.analyse(ourfile)  
- 
-    def generator(self):
-        first = 10**(self-1)
-        last = 10**self-1
+        self.digits = 5
+        self.ourfile = "text.txt"
+        self.attlist = []
+        self.trycount = 0
+        self.result = 00
+       
+    def checkdifferent(self, number):
+        for i in range(0, len(number), 1):
+            for j in range(i+1, len(number), 1):
+                if number[i]==number[j]:
+                    return False
+        return True
+    
+    def generator(self, digits):
+        first = 10**(digits-1)
+        last = 10**digits-1
         b=str(random.randint(first,last))
-        print(b)
+        while self.checkdifferent(b) == False:
+            b=str(random.randint(first,last))     
+#        print(b)
         return b
-            
-    def writefile(self, mynum):
-        f = open(self, 'at')
-        f.write(mynum + '\n')
+    
+    def genattempts(self, digits):
+        for num in range(10**(digits-1), 10**digits-1, 1):
+            if self.checkdifferent(str(num))==True:
+                self.attlist=self.attlist + [num]
+#         f = open('attempts.txt', 'wt')
+#         for l in self.attlist:
+#             f.write(str(l) + ' ')
+#         f.close()
+#         print ("attlist: " + str(len(self.attlist)))
+              
+    def writefile(self, filename, data):
+        f = open(filename, 'at')
+        f.write(data + '\n')
         f.close()
     
+#     def getsize(self, filename):    #numbers of strings in file - maybe will be needed
+#         f = open(filename, 'rt')
+#         fd = f.readlines()
+#         print ('size of file: ' + str(len(fd)))
+#         f.close()
     
-    def getsize(self):    
-        f = open(self, 'rt')
+    def genresponse(self, attempt):
+        mytry = str(attempt)
+        bulls=0
+        cows=0
+        for i in range(0, len(mytry), 1):
+            if mytry[i] == self.secret[i]:
+                bulls+=1
+        for i in range(0, len(mytry), 1):
+            for j in range(0, len(mytry), 1):
+                if (mytry[i]==self.secret[i] and i!=j):
+                    cows+=1
+#        print ("Mytry: " + mytry + " Bulls: " + str(bulls) + " Cows: " + str(cows))
+        self.writefile(self.ourfile, str(bulls) + str(cows))
+        
+    def resultprocessing(self, result):
+        print ('Result from file: ' + str(result))    
+        
+    def analyze(self, filename, digits):
+        f = open(filename, 'rt')
         fd = f.readlines()
-        print ('size of file: ' + str(len(fd)))
-        f.close()
-        
-    def analyze(self):
-        f = open(self, 'rt')
-        fd = f.readlines()
-        for line in fd: 
-            if len(line)-1==2: 
-                print ("result: " + line) 
-            else: 
-                print ('my guess: ' + line)
+        line = fd[-1]
+        if len(line)-1==2: 
+            self.resultprocessing(line)
+        elif len(fd[-1])-1==digits: 
+            print ('Last line containes 5 digits')
+        else: 
+            print ('!!! Data format error: ' + line)
+#        print('last line: ' + fd[-1])
+     
+    def cleanlist(self, result):
+        for num in self.attlist:
+            if self.genresponse(num) != self.result:
+                self.attlist.remove(num)
+         
 
-        
-digits = 5
-ourfile = "text.txt"
-mynum = BullsAndCows.generator(digits)
-BullsAndCows.writefile(ourfile, mynum)
-BullsAndCows.getsize(ourfile)
-BullsAndCows.analyze(ourfile)
+A = BullsAndCows()    #Create class object
+A.secret = A.generator(A.digits)    #Generate number to guess
+print('Secret number: ' + A.secret)   #Print secret number
+A.writefile(A.ourfile, A.secret)     # Write secret to file -             to remove
+mytry = A.generator(A.digits)       #Generate first attempt
+A.result = A.genresponse(mytry)        # Generate result of first attempt
+A.genattempts(A.digits)         # Generate the attempts list 
+#A.writefile(A.ourfile, A.res)
+#A.getsize(A.ourfile)    #to remove
+#A.analyze(A.ourfile, A.digits)        #Wait to duel game
+print('before cleaning: ' + str(len(A.attlist)))
+A.cleanlist(A.result)
+print('after cleaning: ' + str(len(A.attlist)))
 
-    
-        
+
+      
